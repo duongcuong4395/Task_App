c705166cc3bd8c848f5953922109b0cbde9c9b53
duong Cuong <dangduongcuong@gmail.com>
2024-08-27 10:10:34 +0700

    Init

---
+++ MyTasks/MyTasks/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyTasks
+//
+//  Created by pc on 27/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ MyTasks/MyTasks/MyTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTasksApp.swift
+//  MyTasks
+//
+//  Created by pc on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
