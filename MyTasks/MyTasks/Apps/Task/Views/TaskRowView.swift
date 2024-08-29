e4b76c2b1c0a1a91abe7f265ab49d2b1dec4eb1b
duong Cuong <dangduongcuong@gmail.com>
2024-08-29 22:20:36 +0700

    Refactor view

--- MyTasks/MyTasks/Apps/Task/Views/TaskRowView.swift
+++ MyTasks/MyTasks/Apps/Task/Views/TaskRowView.swift
@@ -16,12 +16,7 @@ struct TaskRowView: View {
     var task: TaskCD
     
     var body: some View {
-        DisclosureGroup(isExpanded: $isExpanded) {
-            ForEach(task.subtaskArray) { subtask in
-                SubtaskRowView(subtask: subtask)
-                    .padding(.leading, 20)
-            }
-        } label: {
+        if task.subtaskArray.isEmpty {
             HStack {
                 Button(action: {
                     guard let id = task.id else { return }
@@ -69,8 +64,64 @@ struct TaskRowView: View {
                         .font(.title3)
                 }
             }
+            .padding(.vertical, 5)
+        } else {
+            DisclosureGroup(isExpanded: $isExpanded) {
+                ForEach(task.subtaskArray) { subtask in
+                    SubtaskRowView(subtask: subtask)
+                        .padding(.leading, 20)
+                }
+            } label: {
+                HStack {
+                    Button(action: {
+                        guard let id = task.id else { return }
+                        lnManager.removeRequest(with: "\(id)")
+                        
+                        deleteTask()
+                        hapticsManager.warningHaptic()
+                    }, label: {
+                        Image(systemName: "trash.fill")
+                            .foregroundStyle(.red)
+                            .font(.title3)
+                    })
+                    .padding(5)
+                    
+                    VStack(alignment: .leading) {
+                        Text(task.title ?? "No Title")
+                            .font(.headline)
+                        HStack {
+                            AppShare.priorityIcon(priority: task.priority ?? "Medium")
+                                .foregroundColor(AppShare.priorityColor(priority: task.priority ?? "Medium"))
+                            Text(task.priority ?? "Medium")
+                                .font(.caption)
+                            Spacer()
+                        }
+                        HStack {
+                            Text(task.dueDate ?? Date(), style: .date)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                            Text(task.dueDate ?? Date(), style: .time)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                            Spacer()
+                        }
+                        
+                    }
+                    Spacer()
+                    
+                    if task.isCompleted {
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.green)
+                            .font(.title3)
+                    } else {
+                        Image(systemName: "circle")
+                            .foregroundColor(.gray)
+                            .font(.title3)
+                    }
+                }
+            }
+            .padding(.vertical, 5)
         }
-        .padding(.vertical, 5)
     }
     
     func deleteTask() {
